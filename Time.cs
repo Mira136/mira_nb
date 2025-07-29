f3bec975eabdf99f605daa7699ea0c3215f163e2
Mira136 <mirap7365@gmail.com>
2025-07-29 18:28:20 +0530

    commit

---
+++ Time.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace mira_nb
+{
+    public class Time
+    {
+        double hour;
+        double minute;
+        double second;
+        public void getTime(double hour, double minute, double second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+        public void showTime()
+        {
+            Console.WriteLine("Time : " + hour + ":" + minute + ":" + second);
+        }
+    }
+
+    internal class ClassDemo1
+    {
+        public static void Main(string[] args)
+        {
+            Time t1 = new Time();
+            t1.getTime(11, 28, 35);
+            t1.showTime();
+        }
+    }
+}
