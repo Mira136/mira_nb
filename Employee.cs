6180c09db55c9eba89cb55e9b4de372b300e34c8
Mira136 <mirap7365@gmail.com>
2025-07-29 11:16:01 +0530

    program

---
+++ Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mira_nb
+{
+    public class Employee
+    {
+        int empId;
+        string eName;
+        double salary;
+        void getData()
+        {
+            empId = 111;
+            eName = "Mira";
+            salary = 30000.00;
+        }
+        public void showData()
+        {
+            Console.WriteLine("Emp id: " + empId);
+            Console.WriteLine("Emp Name: " + eName);
+            Console.WriteLine("Emp Salary: " +  salary);
+        }
+        internal class ClassDemo
+        {
+            public static void Main(string[] args)
+            {
+                Employee e1 = new Employee();
+                e1.getData();
+                e1.showData();
+
+                Employee e2 = new Employee();
+                e2.getData();
+                e2.showData();
+            }
+        }
+    }
+}
