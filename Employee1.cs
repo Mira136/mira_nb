19aad944b6007f25b4e1fae6a4e5537d32f9ea5a
Mira136 <mirap7365@gmail.com>
2025-08-05 11:29:10 +0530

    commit

---
+++ Employee1.cs
@@ -0,0 +1,30 @@
+﻿using mira_nb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mira_nb
+{
+    public class Employee1()
+    { }
+
+    public void showData()
+        {
+            Console.WriteLine("Employee id is:" + eId);
+            Console.WriteLine("Employee name is:" + eName);
+            Console.WriteLine("Employee salary is:" + eSalary);
+        }
+    }
+
+    internal class DemoClass2
+    {
+         public static void Main(string[] args)
+         {
+              Employee1 emp = new Employee1();
+              emp.showData();
+         }
+    }
+}
+
---
+++ Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mira_nb
+{
+    public class Student
+    {
+        int rlno;
+        string name;
+
+        public Student()//default constructor
+        {
+            //rlno = 110;
+            //name = "AAAA";
+            Console.WriteLine("Enter rollnumber:");
+            rlno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter name of student:");
+            name = Console.ReadLine();
+        }
+
+        public Student(int r, string n) // parameterised constructor
+        {
+            rlno = r;
+            name = n;
+        }
+
+        public Student(Student s)//copy constructor
+        {
+            rlno = s.rlno;
+            name = s.name;
+        }
+
+        /*public Student() // static constructor
+        { 
+            Console.WriteLine("Static constructor is called");
+        }*/
+
+        public void showData()
+        {
+            Console.WriteLine("Roll number is:" + rlno);
+            Console.WriteLine("Name is:" + name);
+        }
+    }
+
+    internal class Class3
+    {
+        public static void Main(string[] args)
+        {
+            /* Student s1 = new Student();//constructer is called here
+               s1.showData();
+               int x = 112;
+               string y = "aaaa";
+               Student s2 = new Student(110,"ABC");
+               s2.showData();
+               Student s3 = new Student(x,y);//const is called
+               s3.showData();
+               Student s4 =  new Student(s2);
+               s4.showData(); */
+
+            Student[] s = new Student[2];
+            for (int i = 0; i < s.Length; i++)
+            {
+                s[i] = new Student();
+            }
+            Console.WriteLine("Student Details are:");
+            for (int i = 0; i < s.Length; i++)
+            {
+                s[i].showData();
+            }
+        }
+    }
+}
